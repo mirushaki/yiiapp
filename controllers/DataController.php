2b0f307b317c4769104c2cbd494a2fe812accdc6
miriani <mirianjinchvelashvili@gmail.com>
2018-12-24 16:18:53 +0400

    Fix ActiveRecord CRUD

--- controllers/DataController.php
+++ controllers/DataController.php
@@ -75,18 +75,14 @@ class DataController extends Controller
     {
         $request = \Yii::$app->request;
 
-        $user = new Users();
+        $user = Users::find()
+            ->where(['id' => $id])
+            ->one();
+
+        if(!$user)
+            $user = new Users();
 
         if($request->isGet) {
-            if ($id) {
-                try {
-                    $user = Users::find()
-                        ->where(['id' => $id])
-                        ->one();
-                } catch (\Throwable $e) {
-                    Throw $e;
-                }
-            }
             return $this->render('userForm', ['user' => $user]);
         }
         else if($request->isPost)
@@ -95,69 +91,7 @@ class DataController extends Controller
             {
                 try
                 {
-                    $userExists = Users::find()
-                        ->where('id = :id', [':id' => $user->getAttribute('id')])
-                        ->exists();
-
-                    if(!$userExists)
-                    {
-                        $user->insert(true);
-                        \Yii::$app->session->setFlash('message', 'A new user has been created successfully!');
-                        \Yii::$app->session->setFlash('message-class', 'alert-success');
-                    }
-                    else
-                    {
-                        if($user->update(true) !== false)
-                        {
-                            \Yii::$app->session->setFlash('message', 'An existing user has been updated successfully!');
-                            \Yii::$app->session->setFlash('message-class', 'alert-info');
-                        }
-                        else
-                        {
-                            \Yii::$app->session->setFlash('message', 'Failed to update existing user');
-                            \Yii::$app->session->setFlash('message-class', 'alert-danger');
-                        }
-                    }
-
-                    /*
-                    $db->open();
-                    $requestedUser = false;
-
-                    if(($user->id) && ($user->id != 0)) {
-                        $requestedUser = Users::find()
-                            ->where(['Id' => $id])
-                            ->limit(1)
-                            ->one();
-                    }
-                    if($requestedUser)
-                    {
-                        Users::findOne($user->id)->update(false);
-
-                        $db->createCommand()->update('Users', [
-                            'firstName' => $user->firstName,
-                            'lastName' => $user->lastName,
-                            'eMail' => $user->eMail
-                        ], 'Id = :id', [':id' => $user->id])->execute();
-
-                        //$db->createCommand("Update Users SET firstName='$user->firstName', lastName='$user->lastName',
-                                                      //eMail='$user->eMail' WHERE Id = $user->id")->execute();
-                        \Yii::$app->session->setFlash('message', 'An existing user has been updated successfully!');
-                        \Yii::$app->session->setFlash('message-class', 'alert-info');
-                    }
-                    else
-                    {
-                        $db->createCommand()->insert('Users', [
-                            'firstName' => $user->firstName,
-                            'lastName' => $user->lastName,
-                            'eMail' => $user->eMail]
-                        )->execute();
-
-                        //$db->createCommand("insert into Users(firstName, lastName, eMail)
-                                        //values ('$user->firstName', '$user->lastName', '$user->eMail')")->execute();
-                        \Yii::$app->session->setFlash('message', 'A new user has been created successfully!');
-                        \Yii::$app->session->setFlash('message-class', 'alert-success');
-                    }
-                    $db->close();*/
+                    $user->save();
                 }
                 catch (\Throwable $e) {
                     \Yii::$app->session->setFlash('message', 'Failed to add a new user!');
